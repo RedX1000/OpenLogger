d565626e70174105bd23b1879091d93d4aacf326
RedX1000 <clwms16@gmail.com>
2022-03-30 11:32:26 -0500

    Simplified compareItems,implemented readQuantities

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import * as resemble from "resemblejs";
 import compareImages from "resemblejs/compareImages"
 import * as lsdb from './JSONs/LocalStorageInit.json';
 import * as items from './JSONs/ItemsAndImages.json';
+import { IgnorePlugin } from "webpack";
+import { imageDataFromBase64 } from "@alt1/base/dist/imagedetect";
 
 //tell webpack to add index.html and appconfig.json to output
 require("!file-loader?name=[name].[ext]!./index.html");
@@ -15,7 +17,8 @@ var rewardSlots = ["first_item", "second_item", "third_item", "fourth_item", "fi
 var tierlist = ["easy", "medium", "hard", "elite", "master"]
 var listOfItems
 var listOfItemsArray = []
-var firstiter = false
+var legacy = false
+var displaybox = true
 
 export function refresh(){
 	location.reload();
@@ -110,7 +113,10 @@ export function changeClueTierSpan(id){
 //this function is async, so you cant acccess the images instantly but generally takes <20ms
 //use `await imgs.promise` if you want to use the images as soon as they are loaded
 var imgs = a1lib.ImageDetect.webpackImages({
-	trailComplete: require("./images/TrailComplete.data.png")
+	trailComplete: require("./images/TrailComplete.data.png"),
+	trailCompleteLegacy: require("./images/TrailCompleteLegacy.data.png"),
+	rewardValue: require("./images/RewardValue.data.png"),
+	rewardValueLegacy: require("./images/RewardValueLegacy.data.png")
 });
 
 //listen for pasted (ctrl-v) images, usually used in the browser version of an app
@@ -134,34 +140,66 @@ export function capture() {
 }
 
 async function findtrailComplete(img: ImgRef) {
-	var loc = img.findSubimage(imgs.trailComplete);
-
+	try{
+		var loc = img.findSubimage(await imgs.trailCompleteLegacy);
+		console.log(loc[0].x);
+		console.log("Legacy window");
+		legacy = true;
+	} catch(e){
+		var loc = img.findSubimage(await imgs.trailComplete);
+		console.log(loc[0].x);
+		console.log("Non-legacy window");
+		legacy = false;
+	}
+	
 	//overlay the result on screen if running in alt1
 	if (window.alt1) {
 		if (loc.length != 0) {
-			alt1.overLayRect(a1lib.mixColor(255,0,0), loc[0].x - 27, loc[0].y - 13, imgs.trailComplete.width + 278, imgs.trailComplete.height + 213, 2000, 3);
-		} else {
-			alt1.overLayTextEx("Couldn't find Rewards window. Remove\n    any obstructions or open a casket.", a1lib.mixColor(255, 255, 255), 20, Math.round(alt1.rsWidth / 2), 200, 4000, "", true, true);
+			if (displaybox){
+				if(!legacy){
+					alt1.overLayRect(a1lib.mixColor(255,0,0), loc[0].x - 27, loc[0].y - 13, await imgs.trailComplete.width + 278, await imgs.trailComplete.height + 213, 2000, 3);
+					//alt1.overLayRect(a1lib.mixColor(255,0,50), roc[0].x, roc[0].y, await imgs.rewardValue.width, await imgs.rewardValue.height, 2000, 3);
+				}
+				else{	// Offset for x is 111
+					alt1.overLayRect(a1lib.mixColor(0,255,0), loc[0].x - 138, loc[0].y - 13, await imgs.trailCompleteLegacy.width + 278, await imgs.trailCompleteLegacy.height + 213, 2000, 3);
+					//alt1.overLayRect(a1lib.mixColor(255,0,50), roc[0].x, roc[0].y, await imgs.rewardValueLegacy.width, await imgs.rewardValueLegacy.height, 2000, 3);
+				} 
+			}
+			else {
+				alt1.overLayTextEx("Couldn't find Rewards window. Remove\n    any obstructions or open a casket.", a1lib.mixColor(255, 255, 255), 20, Math.round(alt1.rsWidth / 2), 200, 4000, "", true, true);
+			}
 		}
-	}
+	}   
 
 	//get raw pixels of image and show on screen (used mostly for debug)  
-	var buf = img.toData(loc[0].x - 27, loc[0].y - 13, imgs.trailComplete.width + 278, imgs.trailComplete.height + 213);
+	var buf = img.toData(loc[0].x - 27, loc[0].y - 13, await imgs.trailComplete.width + 278, await imgs.trailComplete.height + 213);
+	//var ruf = img.toData(roc[0].x, roc[0].y, await imgs.rewardValue.width, await imgs.rewardValue.height);
 	//buf.show();
 
 	console.log("About to run array...");
 	let crops = new Array<ImageData>(9);
+	let topCrops = new Array<ImageData>(9);
 	// Tweak these two values below if jagex adjusts the pixel placement of the items
-	var x1 = loc[0].x - 1;
-	var y1 = loc[0].y + 39;
+	if(!legacy){
+		var x1 = loc[0].x - 1;
+		var y1 = loc[0].y + 39;
+	}
+	else{
+		var x1 = loc[0].x - 112;
+		var y1 = loc[0].y + 39;
+	}
 	for(let i = 0; i < crops.length; i++) {
 		//console.log("In array check...");
-		crops[i] = img.toData(x1, y1, 32, 32,);
-		alt1.overLayRect(a1lib.mixColor(255,144,0), x1, loc[0].y + 39, 32, 32, 2000, 1);
+		crops[i] = img.toData(x1, y1, 32, 32);
+		topCrops[i] = img.toData(x1, loc[0].y + 41, 32, 8);
+		if(displaybox){
+			alt1.overLayRect(a1lib.mixColor(255,144,0), x1, loc[0].y + 39, 32, 32, 2000, 1);
+			alt1.overLayRect(a1lib.mixColor(255,144,20), x1, loc[0].y + 41, 32, 8, 2000, 1);
+		}
 		x1 += 40;
 
 		// Displaying in Rewards Capture
-		document.getElementById(rewardSlots[i]).textContent = ""
+		document.getElementById(rewardSlots[i]).textContent = "";
 		var canvas = document.createElement("canvas");
 		var ctx = canvas.getContext("2d");
 		var imgvar = document.createElement("img");
@@ -174,10 +212,8 @@ async function findtrailComplete(img: ImgRef) {
 	}
 	
 	console.log("Checking items...");
-	console.log(listOfItemsArray.length);
 	var itemresults = []
 	let promises = []
-	
 	for(let i = 0; i < 9; i++){
 		try{
 			promises.push(itemresults.push(await compareItems(crops[i])));
@@ -195,107 +231,220 @@ async function findtrailComplete(img: ImgRef) {
 	for(let i = 0; i < 9; i++){
 		if(itemresults[i] == "Blank")
 			continue;
-		promises.push(quantresults.push(await readQuantites(crops[i])));
+		promises.push(quantresults.push(await readQuantites(topCrops[i])));
 	}
 	await Promise.all(promises)
 	console.log(quantresults)
+
+
 }
 	
 async function compareItems(item:ImageData){
+	//TODO: Try to get Legacy to work properly
+	//Legacy is a bit janky right now, it can't identify blank
+	//spaces and it can't identify some items...
+	
 	// Take copy of the original array
 	let matches = listOfItemsArray.slice();
 
 	// Can't use all at once. Can only do one color at a time.
-	const yellow = {
-		r: 255,
-		g: 0,
-		b: 0,
-		a: 255
-	};
-	const black1 = {
-		r: 0,
-		g: 0,
-		b: 0,
-		a: 255
-	};
-	const black2 = {
-		r: 0,
-		g: 0,
-		b: 1,
-		a: 255
-	};
-	const black3 = {
-		r: 0,
-		g: 0,
-		b: 2,
-		a: 255
-	};
-	let colors = [yellow, black1, black2, black3]
-	// Just hold this for now just in case...
+	// const yellow = { r: 255, g: 0, b: 0, a: 255};
+	// const black1 = { r: 0, g: 0, b: 0, a: 255};
+	// const black2 = { r: 0, g: 0, b: 1, a: 255};
+	// const black3 = { r: 0, g: 0, b: 2, a: 255};
+	// const legacytan = { r: 62, g: 53, b: 40, a: 255};
+	// const rs3blue = { r: 10, g: 31, b: 41, a: 255};
 
-	var imgdata = await compareImages(item, matches[0][1] , {output: {}, ignore: "less"})
-	matches[0][2] = imgdata.misMatchPercentage;
-	if(matches[0][2] == 0.00){ // If it is blank
-		return "Blank";
+	// let colors = [yellow, black1, black2, black3]
+	// Just hold this for now just in case...
+	
+	if(!legacy){ //Legacy interface?
+		var imgdata = await compareImages(item, matches[0][1] , {output: {}, ignore: "less"})
+		matches[0][2] = imgdata.rawMisMatchPercentage;
+		if(matches[0][2] == 0.00)
+			return "Blank"
+	}
+	else{ //Legacy interface.
+		var imgdata = await compareImages(item, matches[1][1] , {output: {}, ignore: ["less", "colors"]})
+		matches[1][2] = imgdata.rawMisMatchPercentage;
+		if(matches[1][2] == 0.00) // If it is blank
+			return "Blank";
 	}
 
 	matches.shift() // Remove blank if not blank
-	const promises = []
-	for(let i = 0; i < matches.length; i++)
-		//	{output: {ignoreAreasColoredWith: colors}}
-		// 	Choices are: yellow, black1, black2, black3
-		promises.push(await compareImages(item, matches[i][1] , {output: {}, ignore: "less"} ).then(data => {
-			matches[i][2] = data.misMatchPercentage;
-		}));
-	await Promise.all(promises)
-	
-	let precision = 30.00 // The lower the closer the match
-	let totalDiscovered = 0 
-	let found = ""
-	while(found == "" || precision > 0){ // If item isnt found and precision falls below 0
-		for(let i = matches.length-1; i >= 0; i --){
-			if(matches[0][0] == matches[i][0]) // First item equivalent to the other items?
-				totalDiscovered += 1
-			else // If not, leave this loop
-				break;
-		}
-		if(found != ""){ // If item found, leave
-			break;
-		}
-		else if(totalDiscovered == matches.length){ // If all items are the same in the array, leave
-			found = matches[0][0]
-			break;
-		}
-		else // If not found, remove items from the array that are above precision
-			var i = matches.length
-			while(i--)
-				if(matches[i][2] > precision)
-					matches.splice(i, 1)
-		
-		// Various tweakable precision values at different thresholds
-		if(precision > 10)
-			precision -= 1;
-		else if(precision > 5)
-			precision -= 0.5;
-		else if(precision > 2.5)
-			precision -= 0.25;
-		else if(precision > 1)
-			precision -= 0.1;
-		else if(precision > 0)
-			precision -= 0.01;
-		else
-			break;
-		totalDiscovered = 0
+	matches.shift() // Remove blank if not blank
+	//	{output: {ignoreAreasColoredWith: colors}}
+	// 	Choices are: yellow, black1, black2, black3, legacytan, rs3blue	
+	if(!legacy){
+		var found = matches[0]
+		const promises = []
+		for(let i = 0; i < matches.length; i++)
+			promises.push(await compareImages(item, matches[i][1] , {output: {}, ignore: "less"} ).then(data => {
+				matches[i][2] = data.rawMisMatchPercentage;
+				if(found[2] > matches[i][2])
+					found = matches[i]
+			}));
+		await Promise.all(promises)
 	}
-	if(found != "") // If found return item
-		return found
-	else // If not, return blank
-		return "Blank"
-	//console.log("Out of while loop")
+	else{
+		var found = matches[0]
+		const promises = []
+		for(let i = 0; i < matches.length; i++)
+			promises.push(await compareImages(item, matches[i][1] , {output: {}, ignore: "less"} ).then(data => {
+				matches[i][2] = data.rawMisMatchPercentage;
+				if(found[2] > matches[i][2])
+					found = matches[i]
+			}));
+		await Promise.all(promises)
+	}
+	return found[0]
+	
+	//	I have a smart friend. Below this is my solution and
+	// 	this one here is his.
+	// let found = matches[0]
+	// for(let i = 0; i < matches.length; i++) // Get the closest match.
+		
+	
+	
+	//console.log(matches)
+	//let precision = 30.00 // The lower the closer the match
+	//let totalDiscovered = 0 
+	//let found = ""
+	//while(found == "" || precision > 0){ // If item isnt found and precision falls below 0
+	//	for(let i = matches.length-1; i >= 0; i --){
+	//		if(matches[0][0] == matches[i][0]) // First item equivalent to the other items?
+	//			totalDiscovered += 1
+	//		else // If not, leave this loop
+	//			break;
+	//	}
+	//	if(found != ""){ // If item found, leave
+	//		break;
+	//	}
+	//	else if(totalDiscovered == matches.length){ // If all items are the same in the array, leave
+	//		found = matches[0][0]
+	//		break;
+	//	}
+	//	else // If not found, remove items from the array that are above precision
+	//		var i = matches.length
+	//		while(i--)
+	//			if(matches[i][2] > precision)
+	//				matches.splice(i, 1)
+	//	
+	//	// Various tweakable precision values at different thresholds
+	//	if(precision > 10)
+	//		precision -= 1;
+	//	else if(precision > 5)
+	//		precision -= 0.5;
+	//	else if(precision > 2.5)
+	//		precision -= 0.25;
+	//	else if(precision > 1)
+	//		precision -= 0.1;
+	//	else if(precision > 0)
+	//		precision -= 0.01;
+	//	else
+	//		break;
+	//	totalDiscovered = 0
+	//}
+	//if(found != "") // If found return item
+	// return found
+	//else // If not, return blank
+	//	return "Blank"
 }
 
 async function readQuantites(item: ImageData){
 	// Now to figure out how to read the quantities...
+	let itemCan = document.createElement("canvas");
+	let itemCon = itemCan.getContext('2d');
+	itemCan.width = item.width
+	itemCan.height = item.height
+	itemCon.putImageData(item, 0, 0);
+	var itemImg = new Image();
+	itemImg.src = itemCan.toDataURL("image/png")
+	itemCon.drawImage(itemImg, 0, 0)
+	let pixels = itemCon.getImageData(0, 0, item.width, item.height)
+	console.log(pixels)
+	let pixarr = []
+	let pixeldata = 0
+	for(let i = 0; i < 8; i++){
+		let arr2 = []
+		for(let j = 0; j < 32; j++){
+			let vals = {r: pixels.data[pixeldata], g: pixels.data[pixeldata+1], b: pixels.data[pixeldata+2], a: pixels.data[pixeldata+3]}
+			pixeldata += 4
+			arr2.push(vals)
+		}
+		pixarr.push(arr2)
+	}
+	console.log("Pixel 2D array: ",pixarr)
+	console.log(pixarr[0][0].b)
+	
+	let pixelCount = 0
+	let streak = 0
+	let longestStreak = 0
+	let yellowInCol = false
+	let noYellowStreak = 0
+	let numbers = ""
+	console.log(pixarr.length, pixarr[0].length)
+	for(let i = 0; i < pixarr[0].length; i++){
+		if(noYellowStreak == 3)
+			break;
+		for(let j = 0; j < pixarr.length; j++){
+			if(pixarr[j][i].r == 255 && pixarr[j][i].g == 255 && pixarr[j][i].b == 0 || pixarr[j][i].r == 255 && pixarr[j][i].g == 255 && pixarr[j][i].b == 255){
+				pixelCount++
+				streak++
+				noYellowStreak = 0
+				yellowInCol = true
+				if(streak > longestStreak)
+					longestStreak = streak;
+			}
+			else
+				streak = 0
+		}
+		if(pixelCount == 0)
+			noYellowStreak++;
+		else if(yellowInCol == false){
+			if(pixelCount == 11){
+				if(longestStreak == 3)
+					numbers += "7"
+				else // 9
+					numbers += "1"
+			}
+			else if(pixelCount == 13){
+				if(longestStreak == 3)
+					numbers += "3"
+				else //if 6
+					numbers += "4"
+			}
+			else if(pixelCount == 14)
+				numbers += "0"
+			else if(pixelCount == 15)
+				if(longestStreak == 3)
+					numbers += "2"
+				else if(longestStreak == 4)
+					numbers += "5"
+				else if(longestStreak == 7)
+					numbers += "9"
+				else{ //if 8
+					numbers += "K"
+					break;
+				}
+			else if(pixelCount == 18)
+				numbers += "6"
+			else // if pixelCount == 19
+				numbers += "8"
+			
+			longestStreak  = 0;
+			pixelCount = 0;
+			noYellowStreak ++;
+			console.log("DEBUG","numbers is",numbers);
+		}
+		yellowInCol = false;
+	}
+	if(numbers.includes("K"))
+		return parseInt(numbers.slice(0, -1)) * 1000;
+	else if(numbers != "")
+		return parseInt(numbers);
+	else
+		return 1;
 }
 //print text world
 //also the worst possible example of how to use global exposed exports as described in webpack.config.json
